723d535a281e1887c9ed78d4e23672e1e265588b
JuneJuly <guilhermehenrique7273@gmail.com>
2024-10-08 14:46:48 -0400

    feat: exercícios exemplificando uso de comentários
    Utilizando diversos modos de comentário e exemplos de uso cotidianos.

---
+++ Aulas/Aula-002/comentarios.js
@@ -0,0 +1,16 @@
+// Estou escrevendo um comentário, uma notação no meu código.
+// Esse é um comentário de linha única.
+console.log('Olá mundo.') // O código ao lado contém o texto "Olá mundo".
+console.log('Linha 1')
+// console.log('Linha 2')
+console.log('Linha 3')
+/*
+    console.log('Linha 4')
+    console.log('Linha 5')
+*/
+// Esse comentário será ignorado.
+/*
+    Esse é um bloco de comentário
+    podendo conter códigos ou qualquer notação
+    em mais de uma única linha.
+*/
