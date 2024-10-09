491cd0c096f6a7ed1bdd11f199681485cb7ad457
JuneJuly <guilhermehenrique7273@gmail.com>
2024-10-09 15:01:37 -0400

    feat: trabalho com constantes e variáveis
    Se utilizando de somas e multiplicações, foram feitos alguns exercícios com as tags variáveis 'const' e 'let'

---
+++ Aulas/Aula-004/index.js
@@ -0,0 +1,9 @@
+const valor1 = 5;
+const valor2 = 2;
+const resultado = valor1 * valor2;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+
+console.log(resultado, ',', resultadoDuplicado, ',', resultadoTriplicado);
+console.log(typeof(resultadoTriplicado))
