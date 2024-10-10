111d54e8febf0d1f94460aa29b3dd329156e582f
JuneJuly <guilhermehenrique7273@gmail.com>
2024-10-10 16:33:51 -0400

    feat: adição ano nascimento e realização calculos

--- Aulas/Aula-005/index.js
+++ Aulas/Aula-005/index.js
@@ -5,9 +5,14 @@
 
 const nome = 'Luiz Otávio';
 const sobrenome = 'Miranda';
+const idade = 30;
 const peso = 80;
 const altura = 1.80;
 let imc; 
-
+let anoNasci;
+imc = peso / (altura * altura);
+anoNasci = 2024 - idade;
 // imc = peso / (altura * altura)
 
+console.log(nome, sobrenome, 'possui', peso, 'kg e', altura, 'de altura,', 'seu imc corporal é =', imc)
+console.log(nome, sobrenome, 'nasceu em', anoNasci)
