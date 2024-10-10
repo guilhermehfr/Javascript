e21f8569a36b38f323b3fb1966ebb94ae7290123
JuneJuly <guilhermehenrique7273@gmail.com>
2024-10-10 16:28:07 -0400

    feat: declaração das constantes e variáveis

--- Aulas/Aula-005/index.js
+++ Aulas/Aula-005/index.js
@@ -1,5 +1,13 @@
 /*
-    Luiz Otávio Miranda tem 30 anos, possui 84kg
+    Luiz Otávio Miranda tem 30 anos, possui 80kg
     e 1.80m de altura, seu imc corporal é:
 */
+
+const nome = 'Luiz Otávio';
+const sobrenome = 'Miranda';
+const peso = 80;
+const altura = 1.80;
+let imc; 
+
+// imc = peso / (altura * altura)
 
