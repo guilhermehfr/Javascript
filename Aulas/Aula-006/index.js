379da8c03140fefaed83dcea6eeebb3fea659c6b
JuneJuly <guilhermehenrique7273@gmail.com>
2024-10-11 14:36:08 -0400

    feat: exercícios com tipos de dados primitivos
    Exercícios com: Strings, Numbers
    Trabalhando com valores, e utilizando console.log para demonstração final no output.

---
+++ Aulas/Aula-006/index.js
@@ -0,0 +1,13 @@
+const nome = 'joão';        // String
+const sobrenome = 'pedro';  // String
+const idade = 30;           // Number
+const altura = 1.82;        // Number
+
+let peso;                   // Valor = Undefined -> Indefinido, não aponta para nenhum lugar na memória.
+const corDoFundo = null;    // Valor = Null      -> Nulo,       não aponta para nenhum lugar na memória.
+
+const aprovado = false;     // Boolean = True ou False (Fatores lógicos)
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos e ${altura} metros de altura.`);
+console.log(peso, typeof(peso));
+console.log(typeof(nome), nome);
