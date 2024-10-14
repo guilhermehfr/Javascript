1186fd21aba34d82e1b970f304fc5afc587b2ea4
JuneJuly <guilhermehenrique7273@gmail.com>
2024-10-14 16:18:08 -0400

    feat: página com funções e operadores aritméticos
    O exercício de hoje consistia em trabalhar com operadores aritmeticos e utilizar funções, conhecendo nomeclaturas como: Metodo, função, decremento e incremento, parseInt, parseFloat, entre outros.
    
    Objetivei os conteúdos das 3 aulas anteriores assistidas no curso, em 2 documentos simples, contendo oque foi aprendido durante este período.

---
+++ Aulas/Aula-008/js/script.js
@@ -0,0 +1,14 @@
+window.alert('Olá a todos no mundo.');
+window.confirm('Deseja remover essa postagem?');
+window.prompt('Digite o seu nome');
+
+alert('Olá mundo!');
+confirm('Deseja mesmo?');
+
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite outro número');
+num1 = Number(num1);
+num2 = Number(num2);
+let result = num1 + num2;
+
+alert(`O resultado da sua conta é ${result}`);
