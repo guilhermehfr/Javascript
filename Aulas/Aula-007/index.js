d919307b77b74e3963d4256600e535e853ee0496
JuneJuly <guilhermehenrique7273@gmail.com>
2024-10-14 15:01:45 -0400

    feat: transformação de strings para numbers

--- Aulas/Aula-007/index.js
+++ Aulas/Aula-007/index.js
@@ -8,3 +8,23 @@ console.log(conta);
 
 const resultado = (conta + contador) * contador;
 console.log(resultado);
+
+
+//Utilizando parseInt (Números inteiros)
+const num1 = 2;
+const num2 = parseInt('5');
+let num = num1 + num2;
+console.log(num);
+
+
+// Utilizando parseFloat(números decimais)
+const num4 = 2;
+const num5 = parseFloat('5.5');
+let num6 = num4 + num5;
+console.log(num6);
+
+//Utilizando Number (Números Inteiros ou decimais)
+const numA = 2;
+const numB = Number('5.1');
+let numC = numA + numB;
+console.log(numC);
