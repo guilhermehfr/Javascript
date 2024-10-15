9401c3c503d14633f7f3aa42ab92505f3a407146
JuneJuly <guilhermehenrique7273@gmail.com>
2024-10-15 15:46:11 -0400

    feat: exercícios com string.
    Exercicios de concatenação

--- Aulas/Aula-009/index.js
+++ Aulas/Aula-009/index.js
@@ -5,3 +5,11 @@ let varC = 'C';
 [varA, varB, varC] = [varB, varC, varA];
 
 console.log(varA, varB, varC);
+
+//            01234567
+let string = 'céu azul';
+
+console.log(string[7]);
+console.log(string.concat(' em um lindo dia'));
+console.log(string + ' em um lindo dia.');
+console.log(`${string} em um lindo dia.`);
