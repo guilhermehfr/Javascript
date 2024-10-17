71895b12e2b9160534bec06388ce2b220c0daf35
JuneJuly <guilhermehenrique7273@gmail.com>
2024-10-17 16:56:54 -0400

    feat: adicionado boas praticas com o uso de expressões regulares

---
+++ Aulas/Aula-010/index.js
@@ -0,0 +1,8 @@
+let umaString = "Um texto";
+console.log(umaString.search(/[a-z]/));
+console.log(umaString.search([x]));
+console.log(umaString.replace('Um', 'Outro'));
+console.log(umaString.indexOf('texto'));
+console.log(umaString.lastIndexOf('m', 3));
+
+//Uso de expressões regulares
