ab08e40ca2b8c14291a81419a23293cd1baefbbe
JuneJuly <guilhermehenrique7273@gmail.com>
2024-10-29 15:05:24 -0400

    feat: realização de exercícios com numbers e strings.

---
+++ Aulas/Aula-011/index.js
@@ -0,0 +1,18 @@
+let nom1 = 10;
+let nom2 = 5.6912121212121212;
+nom1 = nom1.toString();
+
+// Descobrir o tipo das variáveis
+console.log(typeof(nom1), typeof(nom2));
+
+// Limitar quantas casas decimáis terá o número
+console.log(nom2.toFixed(2));
+
+// verificar se um número é inteiro
+console.log(Number.isInteger(nom2));
+
+// Soma inválida
+let temp = nom1 + 'ola';
+
+// Retorna o valor true se não for um número, e valor false se for.
+console.log(Number.isNaN(temp));
