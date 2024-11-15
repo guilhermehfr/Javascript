bd5c5b377b1c597533d32638f0b05ed8967f8475
JuneJuly <guilhermehenrique7273@gmail.com>
2024-11-15 14:30:47 -0400

    feat: adição comentários ao código da aula-016

--- Aulas/Aula-016/script.js
+++ Aulas/Aula-016/script.js
@@ -1,17 +1,25 @@
-let a = 'A';
-let b = a;
+/*
+Primitivos (imutáveis) - string, number, boolean, undefined, null (bigint, symbol)
+
+Referência (mutável) - array, object, function
+*/
+
+// Primitivos :
+let a = 'A'; // Original
+let b = a;   // Cópia do valor
 console.log(a, b);
 
 a = 'outra coisa';
 console.log (a, b);
 
-let c = [1, 2, 3]; 
-let d = c;
-
+// Referência :
+let c = [1, 2, 3]; // Original
+let d = c;         // Aponta para o mesmo valor que A
+// let d = [...c]     Copia o valor de C, e se torna independente não sofrendo alterações caso C seja alterado ou vice-versa.
 console.log (c, d);
 
 c.push(4);
-console.log(c, d); 
+console.log(c, d); // 1, 2, 3, 4
 
 d.pop();
-console.log(c, d); 
+console.log(c, d); // 1, 2, 3
