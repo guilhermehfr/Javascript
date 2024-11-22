8b53d4e065f29689c7c6c10be84ca1b129226c73
JuneJuly <guilhermehenrique7273@gmail.com>
2024-11-22 14:52:08 -0400

    feat(js): boas práticas com operadores de comparação

---
+++ Aulas/Aula-018/script.js
@@ -0,0 +1,14 @@
+/*
+OPERADORES DE COMPARAÇÃO
+> maior que
+>= maior ou igual a
+< menor que
+<= menor ou igual a
+=== igualdade estrita (valor e tipo)
+!== diferente estrito (valor e tipo)
+*/
+
+let num1 = 10;
+let num2 = '10';
+const comp = num1 !== num2;
+console.log(comp);
