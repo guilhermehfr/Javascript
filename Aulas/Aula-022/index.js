497a9094ff44ffbdbadb83b07565236f72d91529
JuneJuly <guilhermehenrique7273@gmail.com>
2025-01-10 13:47:02 -0400

    feat: pequeno exercício if e else

---
+++ Aulas/Aula-022/index.js
@@ -0,0 +1,7 @@
+const num = 5;
+
+if (num >= 0 && num <= 10) {
+    console.log(`Sim, ele entra entre 0 e 10, seu número é ${num}`);
+} else {
+    console.log(`Não, ele não esta entre 0 e 10, seu número é ${num}`);
+}
